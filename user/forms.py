7985d8bad2ae453eef31676af75ecedec454faaa
TienTran-2706 <tien.minhtran2706@gmail.com>
2024-08-13 11:32:01 +0700

    Setting up settings.py

---
+++ user/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import User
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "profile_picture", "bio"]
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "profile_picture", "bio"]
