19d9a63c4eecf8875dd1ac69e3f3c0da73283c28
TienTran-2706 <tien.minhtran2706@gmail.com>
2024-08-13 11:25:03 +0700

    Adding User models, views, templates

--- user/models.py
+++ user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from base.base_model import BaseModel
 
-# Create your models here.
+class User(BaseModel, AbstractBaseUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    profile_picture = models.ImageField(upload_to="media")
+    bio = models.TextField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = BaseUserManager()
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
--- user/views.py
+++ user/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import CreateView, UpdateView, DetailView
+from django.contrib.auth import login, authenticate
+from .models import User
+from .forms import UserRegistrationForm, UserUpdateForm
 
-# Create your views here.
+
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'user/user_registration.html'
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.set_password(form.cleaned_data['password'])
+        user.save()
+        login(self.request, user)
+        return redirect('user_profile')
+
+
+class UserProfileView(DetailView):
+    model = User
+    template_name='user/user_profile.html'
+    context_object_name = 'user'
+
+class UpdateProfileView(UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    template_name = 'user/user_update.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
